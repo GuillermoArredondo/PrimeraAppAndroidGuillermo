9ae929f8c185a86744fe51831837d240947b9b04
GuillermoArredondo <guillerapc@gmail.com>
2020-10-14 02:40:45 +0200

    Commit 2: Activity de presentacion y creada la de menu (vacia)

--- app/src/main/java/com/example/primeraappandroidguillermo/MainActivity.kt
+++ app/src/main/java/com/example/primeraappandroidguillermo/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.primeraappandroidguillermo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    //Metodo del boton entrar
+    fun entrar(view: View) {
+        val intent = Intent(this,Menu::class.java)
+        startActivity(intent)
     }
 }
