77c68f1e56aa1bb722d8c8a788a28dfc98c92094
GuillermoArredondo <guillerapc@gmail.com>
2020-10-15 03:59:45 +0200

    Commit 6: en feature1, idiomas y comentarios finales.

--- app/src/main/java/com/example/primeraappandroidguillermo/MainActivity.kt
+++ app/src/main/java/com/example/primeraappandroidguillermo/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    //inicia la actividad principal
     override fun onCreateOptionsMenu(menu: Menu):Boolean {
         menuInflater.inflate(R.menu.menu_main,menu)
         return true;
